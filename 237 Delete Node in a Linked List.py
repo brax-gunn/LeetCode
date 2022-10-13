df63f008ca4f9831b74a53989205deef3223df48
Brax Gunn <brax.gunn.99@gmail.com>
2022-10-13 20:20:23 +0530

    Add Solution of Daily LeetCode Challenge

---
+++ 237 Delete Node in a Linked List.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        currentNode = node
+        nextNode = node.next
+        
+        currentNode.val = nextNode.val
+        currentNode.next = nextNode.next
+        
